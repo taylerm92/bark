192b897ee78542831e2d0c914cddd4111ac691dc
taylerm92 <taylerm92@yahoo.com>
2016-11-01 20:42:42 -0400

    Sidenav and Form
    Added the initial form we will be using. Inputed the star rating and put
    it all in the Sidenav. Sidenav can be activated by pressing the logo.

---
+++ form.php
@@ -0,0 +1,50 @@
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> <!-- This is close button for sideNav -->
+  <div class="container formContainer">
+    <form class="form-horizontal" action="insert.php" method="post">
+
+      <div class="form-group">
+        <div class="col-sm-12">
+          <input type="text" class="form-control" style="text-align: center;" id="name" name="name" placeholder="Name of City" required>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-sm-12">
+          <input type="text" class="form-control" style="text-align: center;" id="keyword" name="keyword" placeholder="Keyword" required>
+        </div>
+      </div>
+
+      <div class="stars">
+        <input class="star star-5" id="star-5" type="radio" name="star" value="5"/>
+        <label class="star star-5" for="star-5"></label>
+        <input class="star star-4" id="star-4" type="radio" name="star" value="4"/>
+        <label class="star star-4" for="star-4"></label>
+        <input class="star star-3" id="star-3" type="radio" name="star" value="3"/>
+        <label class="star star-3" for="star-3"></label>
+        <input class="star star-2" id="star-2" type="radio" name="star" value="2"/>
+        <label class="star star-2" for="star-2"></label>
+        <input class="star star-1" id="star-1" type="radio" name="star" value="1"/>
+        <label class="star star-1" for="star-1"></label>
+      </div>
+
+      <div class="col-sm-12">
+        <button id="search" type="submit" class="btn btn-default">Search</button> <!-- button to submit form -->
+      </div>
+
+    </form>
+  </div>
+</div>
+
+
+<script>
+  /* Open the sidenav */
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "100%";
+  }
+
+  /* Close/hide the sidenav */
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+  }
+</script>
--- index.php
+++ index.php
@@ -15,9 +15,20 @@ echo "Database $dbname selected. <br>";
 		<meta charset="UTF-8">
 		<!-- Bootstrap MaxCDN -->
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="/bark/css/style.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"> <!-- css for star rating on form -->
+		<link rel="stylesheet" type="text/css" href="/bark/css/style.css"><!-- css file for everything except form and sidenav -->
+		<link rel="stylesheet" type="text/css" href="/bark/css/form.css"><!-- css file for sidenav and form -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> <!-- bootstrap related javascript -->
 	</head>
 	<body>
+		<?php include 'form.php'; ?>
+		<!-- Use any element to open the sidenav -->
+		<span onclick="openNav()"><img id="logo" src="/bark/images/logo.png" /></span>
+
+		<div id="main">
+			<!-- All Homepage specific html should go here -->
+		</div>
 
 	<!-- Scripts that shouldn't effect page load go right before the closing body tag -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
