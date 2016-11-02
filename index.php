2c9e4fb69dd72b576fcb2d75ec0a011d8f3ea5ea
taylerm92 <taylerm92@yahoo.com>
2016-11-02 15:27:27 -0400

    Added Table for later inputs

--- index.php
+++ index.php
@@ -41,7 +41,32 @@ $db = $con -> $dbname;
 		<div id="main">
 			<!-- All Homepage specific html should go here -->
 			<div id="second" class="container text-center">
-
+				<table class="table table-striped" >
+				 <thead style="text-align:center;">
+					 <tr>
+						 <th>Firstname</th>
+						 <th>Lastname</th>
+						 <th>Email</th>
+					 </tr>
+				 </thead>
+				 <tbody>
+					 <tr>
+						 <td>John</td>
+						 <td>Doe</td>
+						 <td>john@example.com</td>
+					 </tr>
+					 <tr>
+						 <td>Mary</td>
+						 <td>Moe</td>
+						 <td>mary@example.com</td>
+					 </tr>
+					 <tr>
+						 <td>July</td>
+						 <td>Dooley</td>
+						 <td>july@example.com</td>
+					 </tr>
+				 </tbody>
+			 </table>
 			</div>
 			<div id="third" class="container text-center">
 				<div id="map"></div>
