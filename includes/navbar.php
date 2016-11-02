eb51e9336526b36dc9663d5ced1048b8b742a19f
taylerm92 <taylerm92@yahoo.com>
2016-11-02 13:53:18 -0400

    Organize and Style
    Navbar is now included, as well as the transitioning template.

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <span onclick="openNav()"><img id="logo" src="/bark/images/logo.png" /></span>
+    </div>
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#second">Table Results</a></li>
+        <li><a href="#third">Google Maps</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -4,13 +4,13 @@ $dbhost = 'localhost';
 $dbname = 'yelp';
 
 $con = new MongoClient();
-echo "You connected to Mongo Database <br>";
+//echo "You connected to Mongo Database <br>";
 $db = $con -> $dbname;
 
-echo "Database $dbname selected. <br>";
+//echo "Database $dbname selected. <br>";
 ?>
 <!DOCTYPE html>
-<html>
+<html id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
 	<head>
 		<meta charset="UTF-8">
 		<!-- Bootstrap MaxCDN -->
@@ -22,14 +22,29 @@ echo "Database $dbname selected. <br>";
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> <!-- bootstrap related javascript -->
 	</head>
 	<body>
-		<?php include 'form.php'; ?>
+		<?php include 'includes/navbar.php' ?>
+		<?php include 'includes/form.php'; ?>
 		<!-- Use any element to open the sidenav -->
-		<span onclick="openNav()"><img id="logo" src="/bark/images/logo.png" /></span>
+
 
 		<div id="main">
 			<!-- All Homepage specific html should go here -->
+			<div id="second" class="container text-center">
+				asdf
+			</div>
+			<div id="third" class="container text-center">
+				asdf
+			</div>
 		</div>
+		<!-- Footer -->
+		<footer class="text-center" style="padding: 40px;">
+		  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+		    <span class="glyphicon glyphicon-chevron-up"></span>
+		  </a><br><br>
+		  <p>Copyright <a href="" data-toggle="tooltip" title="Visit Arplux">Arplux</a></p>
+		</footer>
 
+		<script type="text/javascript" src="/bark/scripts/scripts.js"></script>
 	<!-- Scripts that shouldn't effect page load go right before the closing body tag -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</body>
